9778ca3f5a1fbc6f1a1a3d894d662e18595c74ec
gaustin15 <35272713+gaustin15@users.noreply.github.com>
2024-01-23 17:52:00 -0500

    adding multitask import to init

--- debiasm/__init__.py
+++ debiasm/__init__.py
@@ -2,3 +2,4 @@ __version__ = "0.0.1"
 
 from .torch_functions import DEBIASM_train_and_pred, DEBIASM_train_and_pred_log_additive
 from .sklearn_functions import DebiasMClassifier, AdaptationDebiasMClassifier
+from .multitask import MultitaskDebiasMClassifier
